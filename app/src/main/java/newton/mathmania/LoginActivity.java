610d9763487a180c90b48974ca0012fa77a2da07
Tobias Rödebäck <tobias.rodeback@gmail.com>
2017-05-08 18:55:25 +0200

    Third commit
    1. Added views for login, main menu and "about"
    
    2. Implemented related logic for each view
    - login functionality
    - pathing for menu buttons
    - functioning webview for "about"
    
    3. Added some very minor additions related to design

--- app/src/main/java/newton/mathmania/LoginActivity.java
+++ app/src/main/java/newton/mathmania/LoginActivity.java
@@ -1,17 +1,39 @@
 package newton.mathmania;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText userName;
+    private EditText userPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Log.i("hadhasdhsa","Hejsan");
-        Log.i("hadhasdhsa", "chr är bäst, yes and");
 
+        userName = (EditText) findViewById(R.id.loginUserName);
+        userPass = (EditText) findViewById(R.id.loginUserPass);
+    }
+
+    public void loginCheck(View v) {
+
+        if (userName.getText().toString().equals("test") && userPass.getText().toString().equals("test")) {
+
+            Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
+            LoginActivity.this.startActivity(intent);
+        } else {
+
+            toastMessage("Invalid username or password. Please try again.");
+        }
+    }
+
+    private void toastMessage (String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
