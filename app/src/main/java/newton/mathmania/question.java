e519738388526502d0aca99db761481fcf38ae82
ChrillxX <exempel91@hotmail.com>
2017-05-09 09:44:21 +0200

    christian
    svårighetsgrad

---
+++ app/src/main/java/newton/mathmania/question.java
@@ -0,0 +1,13 @@
+package newton.mathmania;
+
+
+public class question {
+
+    private final String [Q_1];
+    private final int answer;
+
+    public question(int question, int answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+}
