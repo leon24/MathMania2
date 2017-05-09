a51a44bacf0c7bfae786a6d789406b51d563caf0
leon24 <leon.landvall@gmail.com>
2017-05-09 09:37:15 +0200

    Database Commit

--- app/src/main/java/newton/mathmania/LoginActivity.java
+++ app/src/main/java/newton/mathmania/LoginActivity.java
@@ -4,14 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import newton.mathmania.models.DatabaseModel;
+
 public class LoginActivity extends AppCompatActivity {
+
+    DatabaseModel mDatabaseModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Log.i("hadhasdhsa","Hejsan");
-        Log.i("hadhasdhsa", "chr är bäst, yes and");
+        mDatabaseModel = new DatabaseModel(this);
 
     }
 }
