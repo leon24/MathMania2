37ab6dd4f5503a6345d67ae0bf2abb6329b1504f
remisaado <remi_saado@hotmail.com>
2017-05-14 18:33:13 +0200

    Json commit
    -Temporary Json file added with "answers"
    -Json parsed
    (Buttons not populated yet)

--- app/src/main/java/newton/mathmania/EasyDifficultyActivity.java
+++ app/src/main/java/newton/mathmania/EasyDifficultyActivity.java
@@ -2,6 +2,8 @@ package newton.mathmania;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class EasyDifficultyActivity extends AppCompatActivity {
 
@@ -9,5 +11,28 @@ public class EasyDifficultyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy_difficulty);
+    }
+    public String loadJSONFromAsset()
+    {
+        String json = null;
+        try
+        {
+            InputStream is = getAssets().open("JSON.json");
+
+            int size = is.available();
+
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+        }
+        catch(IOException ex)
+        {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
     }
 }
