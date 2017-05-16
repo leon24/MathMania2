927457f348b83c7719e03b93fc353dd30bef20d8
ChrillxX <exempel91@hotmail.com>
2017-05-16 17:15:38 +0200

    chrille
    databinding... nästan

--- app/src/main/java/newton/mathmania/HardDifficultyActivity.java
+++ app/src/main/java/newton/mathmania/HardDifficultyActivity.java
@@ -1,11 +1,13 @@
 package newton.mathmania;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import newton.mathmania.databinding.ActivityHardDifficultyBinding;
 
 public class HardDifficultyActivity extends AppCompatActivity {
 
@@ -13,6 +15,8 @@ public class HardDifficultyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hard_difficulty);
+        ActivityHardDifficultyBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_hard_difficulty);
+        question guest = new question()
     }
 
 }
