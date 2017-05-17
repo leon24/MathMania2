bc596ef53efed41def883c62855da47488686f7c
ChrillxX <exempel91@hotmail.com>
2017-05-17 12:25:56 +0200

    Dummy data till Hard
    Chrille

--- app/src/main/java/newton/mathmania/models/ViewModel.java
+++ app/src/main/java/newton/mathmania/models/ViewModel.java
@@ -1,13 +1,45 @@
 package newton.mathmania.models;
 
-/**
- * Created by LeonsBärbara on 2017-05-17.
- */
 
 public class ViewModel {
-    public String getTest() {
-        return test;
+
+    //Dummy Data för Hard difficulity
+
+    private String question = "2x4";
+    private String answer = "8";
+    private String decoy1 = "3";
+    private String decoy2 = "7";
+    private String decoy3 = "25";
+    private String decoy4 = "12";
+    private String decoy5 = "49";
+
+
+    public String getQuestion() {
+        return question;
     }
 
-    String test = "hello";
+    public String getAnswer() {
+        return answer;
+    }
+
+    public String getDecoy1() {
+        return decoy1;
+    }
+
+    public String getDecoy2() {
+        return decoy2;
+    }
+
+    public String getDecoy3() {
+        return decoy3;
+    }
+
+    public String getDecoy4() {
+        return decoy4;
+    }
+
+    public String getDecoy5() {
+        return decoy5;
+    }
+    
 }
